6bcb9dc8080ffc192a3677c3d0c40bbc9477ea91
talmeym <mtalmey@gmail.com>
2017-10-08 13:10:45 +0100

    Applied MIT License source comments

--- src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseYmlLoader.java
+++ src/main/java/com/emarte/regurgitator/extensions/mq/CreateJmsResponseYmlLoader.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
 package com.emarte.regurgitator.extensions.mq;
 
 import com.emarte.regurgitator.core.*;
@@ -20,7 +24,7 @@ public class CreateJmsResponseYmlLoader implements YmlLoader<CreateJmsResponse> 
 		String deliveryMode = loadOptionalStr(yaml, JMS_DELIVERY_MODE);
 		String expiration = loadOptionalStr(yaml, JMS_EXPIRATION);
 		String priority = loadOptionalStr(yaml, JMS_PRIORITY);
-		String redelivered = loadOptionalStr(yaml, JMS_REDELiVERED);
+		String redelivered = loadOptionalStr(yaml, JMS_REDELIVERED);
 		String replyTo = loadOptionalStr(yaml, JMS_REPLY_TO);
 		String timestamp = loadOptionalStr(yaml, JMS_TIMESTAMP);
 		return new CreateJmsResponse(response, messageId, type, destination, correlationId, deliveryMode, expiration, priority, redelivered, replyTo, timestamp);
--- src/main/java/com/emarte/regurgitator/extensions/mq/ExtensionsMqYmlPackageMap.java
+++ src/main/java/com/emarte/regurgitator/extensions/mq/ExtensionsMqYmlPackageMap.java
@@ -1,11 +1,17 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
 package com.emarte.regurgitator.extensions.mq;
 
 import com.emarte.regurgitator.core.AbstractYmlPackageMap;
 
 import java.util.*;
 
+import static java.util.Collections.singletonList;
+
 public class ExtensionsMqYmlPackageMap extends AbstractYmlPackageMap {
-	private static final List<String> kinds = Arrays.asList("create-jms-response");
+	private static final List<String> kinds = singletonList("create-jms-response");
 
 	public ExtensionsMqYmlPackageMap() {
 		addPackageMapping(kinds, "com.emarte.regurgitator.extensions.mq");
